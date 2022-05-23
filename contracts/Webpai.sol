ef110051ff0fb1f9bea9b4b65c9f26034c1a8365
hxyro <hxyro0@gmail.com>
2022-05-23 21:51:19 +0530

    contract added

---
+++ contracts/Webpai.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+contract Webpai{
+    struct Message {
+        uint32 id;
+        string topic;
+        address creator_address;
+        string ipfsid;
+        string message;
+        uint created_at;
+    }
+
+    uint32 private idCounter;
+    mapping(string => Message[]) private MessageByTopic;
+        
+    event Submitted(Message message);
+
+    function getChatHistory(string calldata topic) public view returns(Message[] memory) {
+       return MessageByTopic[topic];
+    }
+
+    function addNewMessage(string calldata topic, string calldata message, string calldata ipfsid) public {
+        
+        require(bytes(message).length > 0, "send more money");
+        
+        Message memory newMessage = Message({
+            id: idCounter,
+            topic: topic,
+            creator_address: msg.sender,
+            ipfsid: ipfsid,
+            message: message,
+            created_at: block.timestamp
+        });
+        MessageByTopic[topic].push(newMessage);
+        idCounter++;
+        emit Submitted(newMessage);
+    }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,5 @@
+import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-ethers'
+module.exports = {
+  solidity: '0.8.4',
+}
