f180f46efdbe91e3a89b5f6a68ae2b9d4541026c
hxyro <hxyro0@gmail.com>
2022-05-27 12:45:33 +0530

    deployed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # webpai-smart-contract
+
+0xeB74284c58401278e99c7EB223569d90a7138b45
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,13 @@
 import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-ethers'
+require('dotenv').config()
+
 module.exports = {
   solidity: '0.8.4',
   networks: {
-    hardhat: {
-      chainId: 1337,
+    polygon: {
+      url: 'https://polygon-rpc.com',
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 }
